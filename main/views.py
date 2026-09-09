56d80a174994cdf6cc40084aee979cf2b7f0afc7
afiqah <banin.shula@ui.ac.id>
2026-09-09 17:19:49 +0700

    Implement MVT with experience model

---
+++ main/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from main.models import Experience
+
+
+def show_main(request):
+    context = {
+        "name": "Banin Shula Afiqah Aradena",
+        "npm": "2506604794",
+        "study_program": "S1 Sistem Informasi",
+        "bio": (
+            "Mahasiswa Sistem Informasi Universitas Indonesia yang tertarik "
+            "pada teknologi, data, dan pengembangan produk."
+        ),
+    }
+
+    return render(request, "index.html", context)
+
+
+def show_experience(request):
+    context = {
+        "name": "Banin Shula Afiqah Aradena",
+        "experience_list": Experience.objects.all(),
+    }
+
+    return render(request, "experience.html", context)
